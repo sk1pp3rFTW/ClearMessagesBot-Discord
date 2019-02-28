39352bd15b3e3defcc3caa2f1528763c2214fa75
sk1pp3r <38215577+sk1pp3rFTW@users.noreply.github.com>
2019-02-28 11:50:22 -0600

    Update clear-messages.js

--- clear-messages.js
+++ clear-messages.js
@@ -1,45 +1,31 @@
-/*
-  A bot to clear/delete messages of a channel
-
-  Usage: !clearMessages  ==> clears all messages of
-  that channel on which the command was run
-
-*/
-
-const CLEAR_MESSAGES = '!clearMessages';
+const CLEAR_MESSAGES = '!clearmessages';
 
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-// Token of my bot
 const token = 'NTUwNDAyNTY0MTM1MzIxNjAw.D1h9_Q.3hwy_aNp2fEE3sc0vi6qeY9-92A';
 
 bot.on('ready', () => {
-  console.log('PelicanClearBot is Ready!');
+  console.log('!clearmessages to run...');
   bot.on('message', message => {
     if (message.content == CLEAR_MESSAGES) {
 
-
-      // Only delete messages if the channel type is TextChannel
-      // DO NOT delete messages in DM Channel or Group DM Channel
       if (message.channel.type == 'text') {
         message.channel.fetchMessages()
           .then(messages => {
             message.channel.bulkDelete(messages);
-            messagesDeleted = messages.array().length; // number of messages deleted
+            messagesDeleted = messages.array().length;
 
-            // Logging the number of messages deleted on both the channel and console.
-            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
-            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
+            message.channel.sendMessage("Number of deleted messages: "+messagesDeleted);
+            console.log('Number of deleted messages: '+messagesDeleted)
           })
           .catch(err => {
-            console.log('Error while doing Bulk Delete');
+            console.log('error.');
             console.log(err);
           });
       }
     }
   });
 });
-
 
 bot.login(token);
